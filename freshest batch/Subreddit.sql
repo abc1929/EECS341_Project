6ecfbf9e2df79f9f2b1d56e1ec14e49eba3e2bf0
Jiawei Wu <abc1929@Gmail.com>
2016-04-19 04:57:26 -0400

    all should be good

---
+++ freshest batch/Subreddit.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Subreddit (
+    subreddit VARCHAR(13) CHARACTER SET utf8,
+    banner_link VARCHAR(79) CHARACTER SET utf8,
+    logo_link VARCHAR(79) CHARACTER SET utf8,
+    users_online INT,
+    subscribers INT
+);
+INSERT INTO Subreddit VALUES ('askreddit','http://b.thumbs.redditmedia.com/PXt8GnqdYu-9lgzb3iesJBLN21bXExRV1A45zdw4sYE.png','http://b.thumbs.redditmedia.com/FfqETFhO_F3IZMkajdZ3tQP-sC0qBivEdJDlJo0btVM.png',42861,11093696);
+INSERT INTO Subreddit VALUES ('funny',NULL,NULL,13663,11020738);
+INSERT INTO Subreddit VALUES ('todayilearned',NULL,NULL,8520,10962903);
+INSERT INTO Subreddit VALUES ('pics',NULL,'http://b.thumbs.redditmedia.com/VZX_KQLnI1DPhlEZ07bIcLzwR1Win808RIt7zm49VIQ.png',13813,10875870);
+INSERT INTO Subreddit VALUES ('science',NULL,'http://b.thumbs.redditmedia.com/NYGpyQu_WzaA81QlfiZ4pKz_OTFI5Gz0tLF8CHOzl-I.png',1142,10793198);
+INSERT INTO Subreddit VALUES ('worldnews',NULL,NULL,8628,10785344);
+INSERT INTO Subreddit VALUES ('iama','http://a.thumbs.redditmedia.com/Je9hJkwwkO_gZblEkMwBK3_MG_laF1_uG5nGd-NEXt4.png','http://b.thumbs.redditmedia.com/RqqzLNmhb6ZtUQF9iflcJNHaz3ox3lzfPa_IVsZ3dJY.png',4819,10649048);
+INSERT INTO Subreddit VALUES ('aww','http://b.thumbs.redditmedia.com/zIQDfi6LbflU8YAdY8fmDJxNu5PlNDIhEK9-YWs_l7M.png','http://b.thumbs.redditmedia.com/aKWBgkEo7FnZ1d598QhzMrSZ-J1mVCk2H4kxOiikL8A.png',6124,9355096);
+INSERT INTO Subreddit VALUES ('videos',NULL,'http://b.thumbs.redditmedia.com/anBbzz1v4lBVAJg98aim4X__AmV_y04AR8lVTfq_qyw.png',15258,10188142);
+INSERT INTO Subreddit VALUES ('gaming',NULL,'http://b.thumbs.redditmedia.com/0PgZl68jAxA6T1BH6uvUQ5Bz1F1GrrJLCL8oi2Gz0Ak.png',18315,10148801);
